25ad2df8eabd3d13b311c4333f90447ec67f5880
Peterr181 <piotrzerdzinski089@gmail.com>
2023-02-17 00:45:40 +0100

    comm

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "37cbfe9185mshf96e62b4648164bp1faeadjsnfa5a183619e1",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "cf58e3b3cb69bb54db054090eb3de6ec";
